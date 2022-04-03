a9e875e3998c74a1a7758f05dbe34b04a6a0fd6a
andresajt <91512619+andresajt@users.noreply.github.com>
2022-04-03 16:28:09 +0800

    BST insert

--- App/player_bst.py
+++ App/player_bst.py
@@ -6,6 +6,13 @@
 # Date Created: 03/04/22
 # ----------------------------------------------------------------------
 
+# a. Add a new file to the folder app called player_bst.py. This will contain the class that is the Binary
+# Search Tree.
+# b. Create a class called PlayerBST in the file you just created. Within that class, create the initialiser
+# method and create an instance variable that
+
+from player_bnode import PlayerBNode
+
 
 class PlayerBST:
     """
@@ -26,3 +33,39 @@ class PlayerBST:
         @return: root node
         """
         return self.__root
+
+    def insert(self, player):
+        """
+        Insert player into the BST
+        @param player: player object
+        """
+        # Create new node
+        node = PlayerBNode(player)
+        if self.__root is None:  # If list is empty
+            # Set root to new node
+            self.__root = node
+        else:  # If list is not empty
+            # Set current to root
+            current = self.__root
+            # Set parent to None
+            parent = None
+            # Loop until current is None
+            while current is not None:
+                # Set parent to current
+                parent = current
+                # If player's name is less than current's name
+                if player.name < current.player.name:
+                    # Set current to current's left
+                    current = current.left
+                else:  # If player's name is greater than current's name
+                    # Set current to current's right
+                    current = current.right
+            # If player's name is less than parent's name
+            if player.name < parent.player.name:
+                # Set parent's left to node
+                parent.left = node
+            else:  # If player's name is greater than parent's name
+                # Set parent's right to node
+                parent.right = node
+        # Increase size
+        self.__size += 1
